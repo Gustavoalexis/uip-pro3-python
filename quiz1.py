ae15be2d122eba3249e182c6d8d8bf1d822d1b6c
Gustavo Alexis Batista Rojas <Gustavoalexis@users.noreply.github.com>
2017-09-13 23:16:35 -0500

    Add files via upload

---
+++ quiz1.py
@@ -0,0 +1,17 @@
+print("mi primer juego ")
+palabra = "comida"
+
+for x in range(3):
+
+
+     nombre = input("intenta con una palabra:")
+
+     if nombre.upper() == palabra:
+         print("great! eres lo maximo")
+         break
+     elif nombre.upper() == palabra:
+         print("great! eres lo maximo")
+         break
+     else:
+         print("!fallastes¡  intente de nuevo")
+
